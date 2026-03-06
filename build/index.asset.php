40e18159f2a494fe60aaa0586246a7e9d8ac71a7
tquinonero <tquinonero.web@gmail.com>
2026-03-06 10:39:20 +0100

    build: update compiled assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-element'), 'version' => '84f33bc35cd750835751');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-element'), 'version' => 'ae412462e45baf7b2e91');
